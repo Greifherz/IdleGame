a5e3c873952c6edd380beb03dd8d87c41a599e34
Douglas Luiz Barbara Silva <lorthan@gmail.com>
2024-11-29 17:07:22 +0100

    Some persistence tests for the sake of having them

--- Assets/Tests/EventServiceTests.cs
+++ Assets/Tests/EventServiceTests.cs
@@ -3,192 +3,189 @@ using ServiceLocator;
 using Services.EventService;
 using Services.TickService;
 
-public class EventServiceTests
+namespace Tests
 {
-    private void InitializeEssentials()
+    public class EventServiceTests
     {
-        Locator.Initialize();
-        var TickService = new AsyncTickService();
-        Locator.Current.Register<ITickService>(TickService);
-    }
-
-    [Test]
-    public void VisitorPipelineEventService_CommonEvents_CommonPipeline_Functionality()
-    {
-        InitializeEssentials();
-
-        var EventService = new VisitorPipelinesEventService();
-        EventService.Initialize();
-    
-        var ReceivedEvent = false;
-        
-        var CommonEventHandler = new CommonEventHandler((commonEvent) => { ReceivedEvent = true; });
-        EventService.RegisterListener(CommonEventHandler);
-        
-        EventService.Raise(new CommonEvent());
-        Locator.Current.Get<ITickService>().ManualClock();
-        
-        Assert.IsTrue(ReceivedEvent);
-    }
-    
-    [Test]
-    public void VisitorPipelineEventService_CommonEvents_ViewPipeline_Functionality()
-    {
-        InitializeEssentials();
-
-        var EventService = new VisitorPipelinesEventService();
-        EventService.Initialize();
-    
-        var ReceivedEvent = false;
-        
-        var CommonEventHandler = new CommonEventHandler((commonEvent) => { ReceivedEvent = true; });
-        EventService.RegisterListener(CommonEventHandler,EventPipelineType.ViewPipeline);
-        
-        EventService.Raise(new CommonEvent(),EventPipelineType.ViewPipeline);
-        Locator.Current.Get<ITickService>().ManualClock();
-        
-        Assert.IsTrue(ReceivedEvent);
-    }
-    
-    [Test]
-    public void VisitorPipelineEventService_CommonEvents_GameplayPipeline_Functionality()
-    {
-        InitializeEssentials();
-
-        var EventService = new VisitorPipelinesEventService();
-        EventService.Initialize();
-    
-        var ReceivedEvent = false;
-        
-        var CommonEventHandler = new CommonEventHandler((commonEvent) => { ReceivedEvent = true; });
-        EventService.RegisterListener(CommonEventHandler,EventPipelineType.GameplayPipeline);
-        
-        EventService.Raise(new CommonEvent(),EventPipelineType.GameplayPipeline);
-        Locator.Current.Get<ITickService>().ManualClock();
-        
-        Assert.IsTrue(ReceivedEvent);
-    }
-    
-    [Test]
-    public void VisitorPipelineEventService_CommonEvents_ServicePipeline_Functionality()
-    {
-        InitializeEssentials();
-
-        var EventService = new VisitorPipelinesEventService();
-        EventService.Initialize();
-    
-        var ReceivedEvent = false;
-        
-        var CommonEventHandler = new CommonEventHandler((commonEvent) => { ReceivedEvent = true; });
-        EventService.RegisterListener(CommonEventHandler,EventPipelineType.ServicesPipeline);
-        
-        EventService.Raise(new CommonEvent(),EventPipelineType.ServicesPipeline);
-        Locator.Current.Get<ITickService>().ManualClock();
-        
-        Assert.IsTrue(ReceivedEvent);
-    }
-    
-    [Test]
-    public void VisitorPipelineEventService_CommonEvents_CommonPipeline_WrongPipeline_Functionality()
-    {
-        InitializeEssentials();
-
-        var EventService = new VisitorPipelinesEventService();
-        EventService.Initialize();
-    
-        var ReceivedEvent = false;
-        
-        var CommonEventHandler = new CommonEventHandler((commonEvent) => { ReceivedEvent = true; });
-        EventService.RegisterListener(CommonEventHandler);
-        
-        EventService.Raise(new CommonEvent(),EventPipelineType.GameplayPipeline);
-        Locator.Current.Get<ITickService>().ManualClock();
-        
-        Assert.IsFalse(ReceivedEvent);
-    }
-    
-    [Test]
-    public void VisitorPipelineEventService_Only_Listens_One_Pipeline()
-    {
-        InitializeEssentials();
-
-        var EventService = new VisitorPipelinesEventService();
-        EventService.Initialize();
-    
-        var ReceivedEventCount = 0;
-        
-        var CommonEventHandler = new CommonEventHandler((commonEvent) => { ReceivedEventCount++; });
-        EventService.RegisterListener(CommonEventHandler,EventPipelineType.CommonPipeline);
-        EventService.RegisterListener(CommonEventHandler,EventPipelineType.GameplayPipeline);
-        EventService.RegisterListener(CommonEventHandler,EventPipelineType.ServicesPipeline);
-        EventService.RegisterListener(CommonEventHandler,EventPipelineType.ViewPipeline);
-        
-        EventService.Raise(new CommonEvent(),EventPipelineType.CommonPipeline);
-        Locator.Current.Get<ITickService>().ManualClock();
-        
-        Assert.AreEqual(ReceivedEventCount,1);
-    }
-    
-    [Test]
-    public void VisitorPipelineEventService_Can_Listen_Several_Times_Same_Pipeline()
-    {
-        InitializeEssentials();
-
-        var EventService = new VisitorPipelinesEventService();
-        EventService.Initialize();
-    
-        var ReceivedEventCount = 0;
-        
-        var CommonEventHandler = new CommonEventHandler((commonEvent) => { ReceivedEventCount++; });
-        EventService.RegisterListener(CommonEventHandler);
-        EventService.RegisterListener(CommonEventHandler);
-        EventService.RegisterListener(CommonEventHandler);
-        
-        EventService.Raise(new CommonEvent());
-        Locator.Current.Get<ITickService>().ManualClock();
-        
-        Assert.AreEqual(ReceivedEventCount,3);
-    }
-    
-    [Test]
-    public void GeneralPipeline_Functionality()
-    {
-        InitializeEssentials();
-
-        var EventService = new GeneralEventService();
-        EventService.Initialize();
-    
-        var ReceivedEvent = false;
-        
-        //General should work with just any class
-        EventService.RegisterGeneralListener<EventServiceTests>((tests) =>
+        private void InitializeEssentials()
         {
-            ReceivedEvent = true;
-        } );
-        
-        EventService.RaiseGeneralEvent(this);
-        
-        Assert.IsTrue(ReceivedEvent);
-    }
-    
-    [Test]
-    public void GeneralPipeline_Only_Notified_Of_Listened_Class_Functionality()
-    {
-        InitializeEssentials();
+            Locator.Initialize();
+            var TickService = new AsyncTickService();
+            Locator.Current.Register<ITickService>(TickService);
+        }
 
-        var EventService = new GeneralEventService();
-        EventService.Initialize();
-    
-        var ReceivedEvent = false;
-        
-        //General should work with just any class
-        EventService.RegisterGeneralListener<EventServiceTests>((tests) =>
+        [Test]
+        public void VisitorPipelineEventService_CommonEvents_CommonPipeline_Functionality()
         {
-            ReceivedEvent = true;
-        } );
-        
-        EventService.RaiseGeneralEvent(EventService);
-        
-        Assert.IsFalse(ReceivedEvent);
+            InitializeEssentials();
+
+            var EventService = new VisitorPipelinesEventService();
+            EventService.Initialize();
+
+            var ReceivedEvent = false;
+
+            var CommonEventHandler = new CommonEventHandler((commonEvent) => { ReceivedEvent = true; });
+            EventService.RegisterListener(CommonEventHandler);
+
+            EventService.Raise(new CommonEvent());
+            Locator.Current.Get<ITickService>().ManualClock();
+
+            Assert.IsTrue(ReceivedEvent);
+        }
+
+        [Test]
+        public void VisitorPipelineEventService_CommonEvents_ViewPipeline_Functionality()
+        {
+            InitializeEssentials();
+
+            var EventService = new VisitorPipelinesEventService();
+            EventService.Initialize();
+
+            var ReceivedEvent = false;
+
+            var CommonEventHandler = new CommonEventHandler((commonEvent) => { ReceivedEvent = true; });
+            EventService.RegisterListener(CommonEventHandler, EventPipelineType.ViewPipeline);
+
+            EventService.Raise(new CommonEvent(), EventPipelineType.ViewPipeline);
+            Locator.Current.Get<ITickService>().ManualClock();
+
+            Assert.IsTrue(ReceivedEvent);
+        }
+
+        [Test]
+        public void VisitorPipelineEventService_CommonEvents_GameplayPipeline_Functionality()
+        {
+            InitializeEssentials();
+
+            var EventService = new VisitorPipelinesEventService();
+            EventService.Initialize();
+
+            var ReceivedEvent = false;
+
+            var CommonEventHandler = new CommonEventHandler((commonEvent) => { ReceivedEvent = true; });
+            EventService.RegisterListener(CommonEventHandler, EventPipelineType.GameplayPipeline);
+
+            EventService.Raise(new CommonEvent(), EventPipelineType.GameplayPipeline);
+            Locator.Current.Get<ITickService>().ManualClock();
+
+            Assert.IsTrue(ReceivedEvent);
+        }
+
+        [Test]
+        public void VisitorPipelineEventService_CommonEvents_ServicePipeline_Functionality()
+        {
+            InitializeEssentials();
+
+            var EventService = new VisitorPipelinesEventService();
+            EventService.Initialize();
+
+            var ReceivedEvent = false;
+
+            var CommonEventHandler = new CommonEventHandler((commonEvent) => { ReceivedEvent = true; });
+            EventService.RegisterListener(CommonEventHandler, EventPipelineType.ServicesPipeline);
+
+            EventService.Raise(new CommonEvent(), EventPipelineType.ServicesPipeline);
+            Locator.Current.Get<ITickService>().ManualClock();
+
+            Assert.IsTrue(ReceivedEvent);
+        }
+
+        [Test]
+        public void VisitorPipelineEventService_CommonEvents_CommonPipeline_WrongPipeline_Functionality()
+        {
+            InitializeEssentials();
+
+            var EventService = new VisitorPipelinesEventService();
+            EventService.Initialize();
+
+            var ReceivedEvent = false;
+
+            var CommonEventHandler = new CommonEventHandler((commonEvent) => { ReceivedEvent = true; });
+            EventService.RegisterListener(CommonEventHandler);
+
+            EventService.Raise(new CommonEvent(), EventPipelineType.GameplayPipeline);
+            Locator.Current.Get<ITickService>().ManualClock();
+
+            Assert.IsFalse(ReceivedEvent);
+        }
+
+        [Test]
+        public void VisitorPipelineEventService_Only_Listens_One_Pipeline()
+        {
+            InitializeEssentials();
+
+            var EventService = new VisitorPipelinesEventService();
+            EventService.Initialize();
+
+            var ReceivedEventCount = 0;
+
+            var CommonEventHandler = new CommonEventHandler((commonEvent) => { ReceivedEventCount++; });
+            EventService.RegisterListener(CommonEventHandler, EventPipelineType.CommonPipeline);
+            EventService.RegisterListener(CommonEventHandler, EventPipelineType.GameplayPipeline);
+            EventService.RegisterListener(CommonEventHandler, EventPipelineType.ServicesPipeline);
+            EventService.RegisterListener(CommonEventHandler, EventPipelineType.ViewPipeline);
+
+            EventService.Raise(new CommonEvent(), EventPipelineType.CommonPipeline);
+            Locator.Current.Get<ITickService>().ManualClock();
+
+            Assert.AreEqual(ReceivedEventCount, 1);
+        }
+
+        [Test]
+        public void VisitorPipelineEventService_Can_Listen_Several_Times_Same_Pipeline()
+        {
+            InitializeEssentials();
+
+            var EventService = new VisitorPipelinesEventService();
+            EventService.Initialize();
+
+            var ReceivedEventCount = 0;
+
+            var CommonEventHandler = new CommonEventHandler((commonEvent) => { ReceivedEventCount++; });
+            EventService.RegisterListener(CommonEventHandler);
+            EventService.RegisterListener(CommonEventHandler);
+            EventService.RegisterListener(CommonEventHandler);
+
+            EventService.Raise(new CommonEvent());
+            Locator.Current.Get<ITickService>().ManualClock();
+
+            Assert.AreEqual(ReceivedEventCount, 3);
+        }
+
+        [Test]
+        public void GeneralPipeline_Functionality()
+        {
+            InitializeEssentials();
+
+            var EventService = new GeneralEventService();
+            EventService.Initialize();
+
+            var ReceivedEvent = false;
+
+            //General should work with just any class
+            EventService.RegisterGeneralListener<EventServiceTests>((tests) => { ReceivedEvent = true; });
+
+            EventService.RaiseGeneralEvent(this);
+
+            Assert.IsTrue(ReceivedEvent);
+        }
+
+        [Test]
+        public void GeneralPipeline_Only_Notified_Of_Listened_Class_Functionality()
+        {
+            InitializeEssentials();
+
+            var EventService = new GeneralEventService();
+            EventService.Initialize();
+
+            var ReceivedEvent = false;
+
+            //General should work with just any class
+            EventService.RegisterGeneralListener<EventServiceTests>((tests) => { ReceivedEvent = true; });
+
+            EventService.RaiseGeneralEvent(EventService);
+
+            Assert.IsFalse(ReceivedEvent);
+        }
     }
 }
---
+++ Assets/Tests/PersistenceServiceTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using NUnit.Framework;
+using Services.PersistenceService;
+
+namespace Tests
+{
+    public class PersistenceServiceTests
+    {
+        [Test]
+        public void PlayerPrefs_Bool_Persistence_Test()
+        {
+            var PersistenceService = new PlayerPrefsPersistenceService();
+            PersistenceService.Initialize();
+
+            var Key = "boolTestKey";
+            
+            PersistenceService.Persist(true,Key);
+
+            var RetrievedInfo = PersistenceService.RetrieveBool(Key);
+            
+            Assert.IsTrue(RetrievedInfo);
+        }
+        
+        [Test]
+        public void PlayerPrefs_Int_Persistence_Test()
+        {
+            var PersistenceService = new PlayerPrefsPersistenceService();
+            PersistenceService.Initialize();
+
+            var Key = "intTestKey";
+            var Value = 224532;
+            
+            PersistenceService.Persist(Value,Key);
+
+            var RetrievedInfo = PersistenceService.RetrieveInt(Key);
+            
+            Assert.AreEqual(RetrievedInfo,Value);
+        }
+        
+        [Test]
+        public void PlayerPrefs_String_Persistence_Test()
+        {
+            var PersistenceService = new PlayerPrefsPersistenceService();
+            PersistenceService.Initialize();
+
+            var Key = "stringTestKey";
+            var Value = "efbewoiEQAQTt4gq4@#%T214BfvSFADGqetge";
+            
+            PersistenceService.Persist(Value,Key);
+
+            var RetrievedInfo = PersistenceService.RetrieveString(Key);
+            
+            Assert.AreEqual(RetrievedInfo,Value);
+        }
+        
+        [Test]
+        public void PlayerPrefs_Float_Persistence_Test()
+        {
+            var PersistenceService = new PlayerPrefsPersistenceService();
+            PersistenceService.Initialize();
+
+            var Key = "floatTestKey";
+            var Value = (float)Math.PI;
+            
+            PersistenceService.Persist(Value,Key);
+
+            var RetrievedInfo = PersistenceService.RetrieveFloat(Key);
+            
+            Assert.AreEqual(RetrievedInfo,Value);
+        }
+    }
+}
