f9a6e5e6dc4421866ae66b45b23a5e08efb575a1
Douglas Luiz Barbara Silva <lorthan@gmail.com>
2025-07-14 16:44:48 +0200

    Scene, initial art, gameplay start

---
+++ Assets/Game/Scripts/Gameplay/Unit/UnitContext.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+
+namespace Game.Gameplay
+{
+    public class UnitContext : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer UnitRenderer;
+        [SerializeField] private TextMeshProUGUI UnitQuantity;
+    }
+    
+}
